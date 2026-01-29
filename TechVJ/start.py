7a93f260efb67c1627d1c73e3160712a7be4c60c
GODZ <godprovider1204@gmail.com>
2026-01-29 22:08:27 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -62,10 +62,10 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("❣️ Developer", url = "https://t.me/kingvj01")
+        InlineKeyboardButton("💞 Dᴇᴠᴇʟᴏᴘᴇʀ", url = "https://t.me/Godz_bots")
     ],[
-        InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/vj_bot_disscussion'),
-        InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/vj_bots')
+        InlineKeyboardButton('Aᴅᴍɪɴ 🕊️', url='https://t.me/Godz_bots'),
+        InlineKeyboardButton('Mᴏʀᴇ Iɴғᴏ 💞', url='https://t.me/Godz_Bots')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
