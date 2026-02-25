248ee30ac411d22d6186cb01723a007a15b9c923
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-02-26 03:27:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ CHANNEL_ID = os.environ.get("CHANNEL_ID", "-1003793547457")
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://Anujofficial:Anujofficial@cluster0.ab123.mongodb.net/mydatabase?retryWrites=true&w=majority") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "Anujedit")
 
 # Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
 WAITING_TIME = int(os.environ.get("WAITING_TIME", "10")) # time in seconds
