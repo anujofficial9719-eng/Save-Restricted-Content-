d79276ea50a5ee4dffd2eb3e7666944b8e960661
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-02-25 06:20:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,25 +14,25 @@ else:
     STRING_SESSION = None
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8427108179:AAHhEN3I4Zup6YmQG6bkL-ekqYbL9eqaoRo")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "21769517"))
+API_ID = int(os.environ.get("API_ID", "34724970"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "a18bca05e643355610f88e15425287a7")
+API_HASH = os.environ.get("API_HASH", "f240eae7c60e8e30c17203ab0e052f7e")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "7562079827"))
+ADMINS = int(os.environ.get("ADMINS", "7521421400"))
 
 # Your Channel Id In Which Bot Upload Downloaded Video/File/Message etc.
 # And Make Your Bot Admin In this channel with full rights.
 # if you don't want to upload in channel then leave it blank don't fill anything.
-CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "-1003793547457")
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://gxmon239:f4l7bKrhka3Fh2cV@cluster0.qmblwql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Anujofficial:Anujofficial@cluster0.ab123.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
